fe97f88843d7e3d5babb342ef209cc9a24ea04e0
StarSheriff <61250665+StarSheriff2@users.noreply.github.com>
2021-01-23 20:15:32 -0600

    add answers for 'SELECT within SELECT Tutorial'

--- SQL_Zoo_answers.sql
+++ SQL_Zoo_answers.sql
@@ -128,4 +128,71 @@ FROM nobel
 WHERE winner LIKE 'Sir%'
 ORDER BY yr DESC
 
+/**
+  ====SELECT within SELECT Tutorial====
+**/
 
+SELECT name FROM world
+  WHERE population >
+     (SELECT population FROM world
+      WHERE name = 'Russia')
+
+SELECT name
+FROM world
+WHERE continent = 'Europe'
+AND gdp/population > (SELECT gdp/population FROM world
+  WHERE name = 'United Kingdom')
+
+SELECT name, continent
+FROM world
+WHERE continent IN (SELECT continent FROM world
+  WHERE name IN ('Argentina', 'Australia'))
+ORDER BY name
+
+SELECT name, population
+FROM world
+WHERE population > (SELECT population FROM world
+  WHERE name = 'Canada')
+AND population < (SELECT population FROM world
+  WHERE name = 'Poland');
+
+SELECT name AS Name, CONCAT(ROUND(population/(SELECT population FROM world
+                      WHERE name = 'Germany')*100),
+    '%') AS Percentage
+FROM world
+WHERE continent = 'Europe';
+
+SELECT name
+FROM world
+WHERE gdp > ALL(SELECT gdp
+                FROM world
+                WHERE continent = 'Europe'
+                AND gdp > 0);
+
+SELECT continent, name, area FROM world x
+  WHERE area >= ALL
+    (SELECT area FROM world y
+        WHERE y.continent=x.continent
+          AND area>0)
+
+SELECT continent, name
+FROM world x
+WHERE name = (SELECT name FROM world y
+              WHERE y.continent = x.continent
+              ORDER BY name
+              LIMIT 1);
+
+SELECT name, continent, population
+FROM world x
+WHERE (
+       SELECT MAX(population)
+       FROM world y
+       WHERE y.continent = x.continent
+      ) <= 25000000;  -- THIS EXCERCISE WAS FUCKING HARD!
+
+SELECT name, continent
+FROM world x
+WHERE population > ALL(SELECT population*3
+                       FROM world y
+                       WHERE y.continent = x.continent
+                       AND y.name <> x.name); -- DAMN, THIS ONE WAS TRICKY!
