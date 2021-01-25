01ae1af6840162e7cb08d30e97fb377bf7500d9d
StarSheriff <61250665+StarSheriff2@users.noreply.github.com>
2021-01-24 21:07:26 -0600

    add answers for 'The JOIN operation'

--- SQL_Zoo_answers.sql
+++ SQL_Zoo_answers.sql
@@ -253,3 +253,68 @@ WHERE (SELECT SUM(population)
        WHERE y.continent = x.continent) >=
        100000000
 GROUP BY continent
+
+/**
+  ====The JOIN operation====
+**/
+
+SELECT matchid, player FROM goal
+  WHERE teamid = 'GER'
+
+SELECT id,stadium,team1,team2
+  FROM game
+WHERE id = 1012
+
+SELECT team1, team2, player
+FROM game JOIN goal
+ON id = matchid
+WHERE player LIKE 'Mario%'
+
+SELECT player, teamid, coach, gtime
+  FROM goal JOIN eteam
+  ON teamid = id
+  WHERE gtime<=10
+
+SELECT mdate, teamname
+FROM game JOIN eteam
+ON game.team1 = eteam.id
+WHERE coach = 'Fernando Santos'
+
+SELECT player
+FROM goal JOIN game
+ON goal.matchid = game.id
+WHERE stadium = 'National Stadium, Warsaw'
+
+SELECT DISTINCT player
+  FROM game JOIN goal ON id = matchid
+    WHERE team1 = 'GER' AND goal.teamid <> 'GER'
+    OR game.team2 = 'GER' AND goal.teamid <> 'GER'
+
+SELECT teamname, COUNT(player)
+  FROM eteam JOIN goal ON id=teamid
+ GROUP BY teamname
+
+SELECT stadium, COUNT(player)
+FROM game JOIN goal
+ON id = matchid
+GROUP BY stadium
+
+SELECT matchid, mdate, COUNT(player)
+FROM goal JOIN game ON matchid = id
+WHERE team1 = 'POL' OR team2 = 'POL'
+GROUP BY matchid, mdate -- THIS ONE WAS FUCKING HARD!
+
+SELECT matchid, mdate, COUNT(player)
+FROM game JOIN goal
+ON id = matchid
+WHERE teamid = 'GER'
+GROUP BY matchid, mdate
+
+SELECT mdate, team1,
+       SUM(CASE WHEN teamid=team1 THEN 1 ELSE 0 END) AS score1,
+       team2,
+       SUM(CASE WHEN teamid=team2 THEN 1 ELSE 0 END) AS score2
+FROM game x LEFT JOIN goal ON x.id = matchid
+WHERE x.id = matchid OR x.id
+GROUP BY mdate, team1, team2
+ORDER BY mdate, matchid, team1, team2 -- I had to use LEFT JOIN for this, which I didn't expect to need at this point
