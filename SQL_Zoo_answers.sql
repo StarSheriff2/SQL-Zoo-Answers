d48dfae786b72155fed90547692e06c9fa16740f
StarSheriff <61250665+StarSheriff2@users.noreply.github.com>
2021-01-26 22:19:27 -0600

    add answers for 'Using Null'

--- SQL_Zoo_answers.sql
+++ SQL_Zoo_answers.sql
@@ -426,3 +426,52 @@ WHERE movieid IN (SELECT movieid FROM casting
                   INNER JOIN actor ON actorid = actor.id
                   WHERE name = 'Art Garfunkel')
 AND name != 'Art Garfunkel';
+
+
+/**
+  ====Using Null====
+**/
+
+SELECT name
+FROM teacher
+WHERE dept IS NULL;
+
+SELECT teacher.name, dept.name
+ FROM teacher INNER JOIN dept
+           ON (teacher.dept=dept.id);
+
+SELECT teacher.name, dept.name
+ FROM teacher LEFT JOIN dept
+           ON (teacher.dept=dept.id)
+
+SELECT teacher.name, dept.name
+ FROM teacher RIGHT JOIN dept
+           ON (teacher.dept=dept.id);
+
+SELECT name, COALESCE(mobile, '07986 444 2266') AS 'mobile number'
+FROM teacher;
+
+SELECT teacher.name AS 'Teacher Name', COALESCE(dept.name, 'None') AS 'Dept Name'
+FROM teacher
+LEFT JOIN dept ON teacher.dept = dept.id;
+
+SELECT COUNT(name), COUNT(mobile)
+FROM teacher
+
+SELECT dept.name AS 'Dept', COUNT(teacher.dept) AS 'Number of Staff'
+FROM teacher
+RIGHT JOIN dept ON teacher.dept = dept.id
+GROUP BY dept.name;
+
+SELECT teacher.name AS 'Teacher name',
+       CASE WHEN dept = 1 OR dept = 2 THEN 'Sci'
+       ELSE 'Art' END AS 'Title'
+FROM teacher LEFT JOIN dept
+ON teacher.dept = dept.id;
+
+SELECT teacher.name AS Teacher,
+       CASE WHEN dept = 1 OR dept = 2 THEN 'Sci'
+            WHEN dept = 3 THEN 'Art'
+            ELSE 'None' END AS 'Title'
+FROM teacher LEFT JOIN dept
+ON teacher.dept = dept.id;
